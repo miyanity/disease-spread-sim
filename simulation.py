50e25e29a1d02a12476a6dd85a8a9a3bda861a35
miyah dones <miymiy@miyahs-MacBook-Pro.local>
2025-07-13 18:12:18 -0400

    initial commit, only person class been iniatiated so far

---
+++ simulation.py
@@ -0,0 +1,15 @@
+# umpire 
+#   u - stimulating how a virus spreads through a population over time
+#   m - plannign to use:
+#           - oop to represent people
+#           - randomness to simulate the spread of a virus
+#           - loops to stimulate days passing
+#           - matplotlib to visualize how things change over time
+#   p
+#   i 
+#   r 
+#   e
+
+class Person:
+    def __init__(self):
+        
